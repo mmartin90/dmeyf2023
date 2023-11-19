9d92c865a0ba2deb0069c912de42d1c8db0a1226
mmartin90 <martinmattiazzi@gmail.com>
2023-11-19 18:04:00 -0300

    Actualizo

--- src/CompetenciaFinal/K03_z823_lightgbm_binaria_BO_FE_02.r
+++ src/CompetenciaFinal/K03_z823_lightgbm_binaria_BO_FE_02.r
@@ -32,7 +32,7 @@ options(error = function() {
 #  muy pronto esto se leera desde un archivo formato .yaml
 PARAM <- list()
 
-PARAM$experimento <- "HT8232_comp3_01"
+PARAM$experimento <- "HT8232_comp3_02"
 
 setwd("~/buckets/b1")
 
@@ -104,7 +104,7 @@ PARAM$bo_lgb <- makeParamSet(
 )
 
 # si usted es ambicioso, y tiene paciencia, podria subir este valor a 100
-PARAM$bo_iteraciones <- 40 # iteraciones de la Optimizacion Bayesiana
+PARAM$bo_iteraciones <- 50 # iteraciones de la Optimizacion Bayesiana
 
 #------------------------------------------------------------------------------
 # graba a un archivo los componentes de lista
@@ -313,17 +313,6 @@ dataset[foto_mes == 201904, mtransferencias_recibidas := NA]
 dataset[foto_mes == 201904, mttarjeta_visa_debitos_automaticos := NA]
 dataset[foto_mes == 201904, Visa_mfinanciacion_limite := NA]
 
-dataset[foto_mes == 201905, ccomisiones_otras := NA]
-dataset[foto_mes == 201905, ctarjeta_visa_debitos_automaticos := NA]
-dataset[foto_mes == 201905, ctransferencias_recibidas := NA]
-dataset[foto_mes == 201905, mactivos_margen := NA]
-dataset[foto_mes == 201905, mcomisiones := NA]
-dataset[foto_mes == 201905, mcomisiones_otras := NA]
-dataset[foto_mes == 201905, mpasivos_margen := NA]
-dataset[foto_mes == 201905, mrentabilidad_annual := NA]
-dataset[foto_mes == 201905, mrentabilidad := NA]
-dataset[foto_mes == 201905, mtransferencias_recibidas := NA]
-
 dataset[foto_mes == 201910, ccajeros_propios_descuentos := NA]
 dataset[foto_mes == 201910, ccomisiones_otras := NA]
 dataset[foto_mes == 201910, chomebanking_transacciones := NA]
@@ -339,48 +328,6 @@ dataset[foto_mes == 201910, mrentabilidad := NA]
 dataset[foto_mes == 201910, mtarjeta_master_descuentos := NA]
 dataset[foto_mes == 201910, mtarjeta_visa_descuentos := NA]
 
-dataset[foto_mes == 202001, cliente_vip := NA]
-
-dataset[foto_mes == 202006, active_quarter := NA]
-dataset[foto_mes == 202006, catm_trx := NA]
-dataset[foto_mes == 202006, catm_trx_other := NA]
-dataset[foto_mes == 202006, ccajas_consultas := NA]
-dataset[foto_mes == 202006, ccajas_depositos := NA]
-dataset[foto_mes == 202006, ccajas_extracciones := NA]
-dataset[foto_mes == 202006, ccajas_otras := NA]
-dataset[foto_mes == 202006, ccajas_transacciones := NA]
-dataset[foto_mes == 202006, ccallcenter_transacciones := NA]
-dataset[foto_mes == 202006, ccheques_depositados := NA]
-dataset[foto_mes == 202006, ccheques_depositados_rechazados := NA]
-dataset[foto_mes == 202006, ccheques_emitidos := NA]
-dataset[foto_mes == 202006, ccheques_emitidos_rechazados := NA]
-dataset[foto_mes == 202006, ccomisiones_otras := NA]
-dataset[foto_mes == 202006, cextraccion_autoservicio := NA]
-dataset[foto_mes == 202006, chomebanking_transacciones := NA]
-dataset[foto_mes == 202006, cmobile_app_trx := NA]
-dataset[foto_mes == 202006, ctarjeta_debito_transacciones := NA]
-dataset[foto_mes == 202006, ctarjeta_master_transacciones := NA]
-dataset[foto_mes == 202006, ctarjeta_visa_transacciones := NA]
-dataset[foto_mes == 202006, ctrx_quarter := NA]
-dataset[foto_mes == 202006, mactivos_margen := NA]
-dataset[foto_mes == 202006, matm := NA]
-dataset[foto_mes == 202006, matm_other := NA]
-dataset[foto_mes == 202006, mautoservicio := NA]
-dataset[foto_mes == 202006, mcheques_depositados := NA]
-dataset[foto_mes == 202006, mcheques_depositados_rechazados := NA]
-dataset[foto_mes == 202006, mcheques_emitidos := NA]
-dataset[foto_mes == 202006, mcheques_emitidos_rechazados := NA]
-dataset[foto_mes == 202006, mcomisiones := NA]
-dataset[foto_mes == 202006, mcomisiones_otras := NA]
-dataset[foto_mes == 202006, mcuentas_saldo := NA]
-dataset[foto_mes == 202006, mextraccion_autoservicio := NA]
-dataset[foto_mes == 202006, mpasivos_margen := NA]
-dataset[foto_mes == 202006, mrentabilidad_annual := NA]
-dataset[foto_mes == 202006, mrentabilidad := NA]
-dataset[foto_mes == 202006, mtarjeta_master_consumo := NA]
-dataset[foto_mes == 202006, mtarjeta_visa_consumo := NA]
-dataset[foto_mes == 202006, tcallcenter := NA]
-dataset[foto_mes == 202006, thomebanking := NA]
 
 
 # Data Drifting
